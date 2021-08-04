45aaa4256780996d15151d00ada32b07e202a9d1
Azizul Tareq <azizul.tareq@northsouth.edu>
2021-08-05 01:30:58 +0600

    showing how many times this website is viewed

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
 import { Image, Container, Button } from 'react-bootstrap'
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 import { MdEmail } from 'react-icons/md'
 import { GoLocation } from 'react-icons/go'
 import { ImFlickr } from 'react-icons/im'
 import myphoto from './myphoto.PNG'
+import axios from 'axios'
 import './SideBar.css'
 import resume from './resume of Tareq.pdf'
 
-const SideBar = () => (
+const SideBar = () => {
+
+  const [viewers, setViewers] = useState(0)
+
+    useEffect(() => {
+      const getViewers = async () => {
+      try {
+        const counter = await axios.get('https://api.countapi.xyz/update/azizultareq.netlify.com/b8f05964-0e9b-46a8-93b0-b6790140646a/?amount=1');
+        setViewers(counter.data.value)
+      } catch(err){
+        console.log(err)
+      }
+    }
+    getViewers();
+    
+  }, []);
+
+  return (
   <Container id="sidebar" style={{backgroundColor: 'rgb(240, 238, 238)', height: '650px'}}>
     <section id="intro">
-      <br />
       <br />
       <br />
       <br />
@@ -36,8 +52,10 @@ const SideBar = () => (
       <a className="all_links"  href="https://www.flickr.com/photos/193326444@N02/" target="__blank"><ImFlickr /> Flickr</a>
       </p>
     </section>
+    <p><span className="viewer-text">Portfolio Views</span> <span className="viewers">{viewers}</span></p>
     <br />
   </Container>
 );
+  }
 
 export default SideBar;
