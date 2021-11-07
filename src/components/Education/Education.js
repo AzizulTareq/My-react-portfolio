262ffedbdd5ef7809e6f348d9881ad803bafa079
Azizul Hakim Tareq <azizul.tareq@northsouth.edu>
2021-11-07 11:51:56 +0600

    working expericen updated

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -16,18 +16,8 @@ const Education = () => {
                 <br /> - Currently developing both frontend and backend of an application where tutors can offer courses/classes and students can enroll in the course. The system will block course fees while enrolling in a course.
                 <br /> - Other features - live class, screen sharing, whiteboard, assign and submit assignments, different functionalities for different users, etc.
                 <br /> - Stack: Nodejs, React, Redux, MongoDB, Bootstrap etc.
-                <br /><span className="date-place">December 2020 - February 2021</span>
+                <br /><span className="date-place">September 2021 - Current</span>
                 <br /><span className="date-place">Dhaka, Bangladesh</span></p>
-                </li>
-            </ul>
-            <ul>
-            <li>
-                <p> <strong>Frontend Developer (Contract)</strong>
-                <br />Prodealz
-                <br /> - Built the frontend of a multi-vendor e-commerce site. Features I added: add to cart, sellers profile & dashboard, product page (carousel & other details).
-                <br /> - Stack: React, Redux, HTML, CSS etc.
-                <br /><span className="date-place">December 2020 - February 2021</span>
-                <br /><span className="date-place">(Remote) Dhaka, Bangladesh</span></p>
                 </li>
             </ul>
             <h3><FaGraduationCap /> Education</h3>
