fa1fbe10df29182430a8837acad06a5f429efd09
Azizul Hakim Tareq <azizultareq7@gmail.com>
2023-09-13 19:10:33 +0600

    update about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,7 +16,7 @@ const About = () => {
         <br />
        <h3>Hi, I'm Azizul Hakim Tareq.</h3>
        <p style={{textAlign: 'justify'}}>I really appreciate the time you took to view my portfolio.<br /><br />
-       I'm a passionate, forward-thinking, and solution-oriented full-stack Software Engineer. I'm looking forward to growing my career by experiencing industry challenges. I've professional experiece of working with Nodejs, React etc. </p>
+       I'm a passionate, forward-thinking, and solution-oriented full-stack Software Engineer. I'm looking forward to growing my career by experiencing industry challenges. I've professional experiece of working with Nodejs, Express.js, React,js, Next.js, MongoDB, PostgreSQL, etc. </p>
      
      
        </Col>
